9872162417424109a1dffac54ebcb586d879e0c0
Solbon13 <Solbon13@yandex.ru>
2021-01-29 22:25:22 +0800

    ref

--- src/Blocks/Filters/Filters.jsx
+++ src/Blocks/Filters/Filters.jsx
@@ -4,10 +4,18 @@ import s from './Filters.module.css'
 import InputField from '../../Components/InputFind/InputFind'
 
 const Filters = () => {
+
+    let filterString =React.createRef()
+
+    let testOnClick = () => {
+        let text = filterString.current.value;
+        alert(text)
+    }
+
     return (
         <div className={s.container}>
-            <button>fdgdf</button>
-            <InputField />
+            <button onClick={testOnClick}>fdgdf</button>
+            <InputField reftest={filterString}/>
         </div>
     )
 }
--- src/Components/InputFind/InputFind.jsx
+++ src/Components/InputFind/InputFind.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import s from './InputFind.module.css'
 
-const InputField = () => {
+const InputField = (props) => {
     return (
         <div>
-            <input type="text" placeholder='Поиск'  className={s.inputFind} />
+            <input type="text" placeholder='Поиск' className={s.inputFind} ref={props.reftest}/>
         </div>
     )
 }
