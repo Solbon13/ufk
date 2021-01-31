8587b00444ad0d40e5b42df2971a6b18211dbdce
Solbon13 <Solbon13@yandex.ru>
2021-01-31 22:40:05 +0800

    reduxWork

--- src/redux/newsReducer.js
+++ src/redux/newsReducer.js
@@ -1,34 +1,30 @@
 const SET_FILTER_TEXT = 'SET_FILTER_TEXT'
 const DO_FILTERING = 'DO_FILTERING'
 
-const setFilterText = (state, text) => {
-    state.filterText = text
-}
-
-const doFiltering = (state) => {
-    state.newsData = state.newsData.filter(newData => {
-        return newData.text.toUpperCase().includes(state.filterText.toUpperCase())
-    })
-}
-
 let initialState = {
     newsData: [
         { id: 1, text: 'text1' },
         { id: 2, text: 'text1' },
         { id: 3, text: 'text1' },
-        { id: 4, text: 'text1 redux' },
+        { id: 4, text: 'redux' },
     ],
     filterText: '1'
 }
 const newsReducer = (state = initialState, action) => {
 
-    if (action.type === SET_FILTER_TEXT){
-        setFilterText(state, action.text)
-        return state
+    if (action.type === SET_FILTER_TEXT) {
+        return {
+            ...state,
+            filterText: action.text
+        }
     }
-    else if (action.type === DO_FILTERING){
-        doFiltering(state)
-        return state
+    else if (action.type === DO_FILTERING) {
+        return {
+            ...initialState,
+            newsData: initialState.newsData.filter(newData => {
+                return newData.text.toUpperCase().includes(state.filterText.toUpperCase())
+            })
+        }
     }
 
     return state
